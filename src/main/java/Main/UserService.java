23f2a8e3d7a5acc30a4a039833977b02c505f3f4
Zeynep <zeynepveziroglu12@gmail.com>
2026-05-08 18:59:46 +0200

    fixat så att klasserna ärver all gemensam kod (admin klasserna ska frtf fixas) & lösenord hashas

--- src/main/java/Main/UserService.java
+++ src/main/java/Main/UserService.java
@@ -1,6 +1,7 @@
 package Main;
 
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -8,26 +9,35 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User saveUser(User user) {
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
         return userRepository.save(user);
     }
 
     public User findByEmail(String email) {
+
         return userRepository.findByEmail(email);
     }
 
     public List<User> findAllUsers() {
+
         return userRepository.findAll();
     }
+
     public void deleteUserById(Long id) {
+
         userRepository.deleteById(id);
     }
     public User findById(Long id) {
+
         return userRepository.findById(id).orElse(null);
     }
 
